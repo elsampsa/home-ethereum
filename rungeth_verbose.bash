a523769b798c8e12a2d5e807b72500feaa7d1c1d
Sampsa Riikonen <sampsa.riikonen@iki.fi>
2021-03-08 16:11:25 +0200

    etc

--- rungeth_verbose.bash
+++ rungeth_verbose.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-go-ethereum/build/bin/geth --syncmode=full --gcmode=full --cache=2048 --maxpeers=50 --verbosity=4 &>tmplog.txt 
+go-ethereum/build/bin/geth --syncmode=full --gcmode=full --cache=2048 --maxpeers=50 --verbosity=4 --snapshot=false --txlookuplimit=0 &>tmplog.txt 
 
